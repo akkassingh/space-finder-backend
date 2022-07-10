b9767abaf7ac3eb99cc9d8969a03249136601993
akkas <akkassingh@gmail.com>
2022-07-11 01:37:12 +0530

    introduction to creating lamnda and API Gateway using cdk.

---
+++ infrastructure/SpaceStack.ts
@@ -0,0 +1,27 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Runtime, Code, Function as LambdaFunction } from 'aws-cdk-lib/aws-lambda';
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { Construct } from 'constructs';
+import { join } from 'path';
+
+export class SpaceStack extends Stack {
+
+    private api = new RestApi(this, 'spaceAPi')
+
+    constructor(scope: Construct, id: string, props: StackProps) {
+        super(scope, id, props)
+
+
+        const helloLambda = new LambdaFunction(this, 'helloLambda', {
+            runtime: Runtime.NODEJS_16_X,
+            code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+            handler: 'hello.main'
+        })
+
+        // Hello API lambda Integration:
+        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+        const helloLambdaResource = this.api.root.addResource('hello');
+        helloLambdaResource.addMethod('GET', helloLambdaIntegration);
+    }
+
+}
