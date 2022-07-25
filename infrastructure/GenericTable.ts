fa726f71dac83dea8d93186f98b93a4c55840d92
akkas <akkassingh@gmail.com>
2022-07-25 23:31:33 +0530

    dynamo db integration: Put an Item

--- infrastructure/GenericTable.ts
+++ infrastructure/GenericTable.ts
@@ -1,21 +1,22 @@
-import { Stack } from "aws-cdk-lib";
-import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Stack } from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 
 export class GenericTable {
+
     private name: string;
     private primaryKey: string;
     private stack: Stack;
     private table: Table;
 
-    public constructor(name: string, primaryKey: string, stack:Stack, ){
+    public constructor(name: string, primaryKey: string, stack: Stack){
         this.name = name;
         this.primaryKey = primaryKey;
         this.stack = stack;
-        this.initilize();
+        this.initialize();        
     }
 
-    private initilize(){
-        this.createTable()
+    private initialize(){
+        this.createTable();
     }
     private createTable(){
         this.table = new Table(this.stack, this.name, {
